4032b62295d6047f010951205c0677f1c81c43e1
win10devoffshore <win10devoffshore@hotmail.com>
2016-10-20 15:30:21 -0700

    add generic message

--- index.js
+++ index.js
@@ -39,8 +39,7 @@ app.post('/webhook', function (req, res) {
             else if (event.message.text == "pay $500 to AT&T") {
                 sendMessage(event.sender.id, { text: "Payment completed successfully" });
             }
-            else if (event.message.text == "Deals")
-            {
+            else if (event.message.text == "Deals") {
                 sendGenericMessage(event.sender.id);
             }
 
@@ -79,12 +78,26 @@ function sendGenericMessage(sender) {
                     {
                         title: 'Hulu',
                         'subtitle': 'Earn $7.99 cash back with your subscription to Hulu. Stream original series, hit shows, movies & more!',
-                        'image_url': 'https://www.android.com/intl/zh-TW_hk/new/images/tv/apps/hulu-plus.png'
+                        'image_url': 'https://www.android.com/intl/zh-TW_hk/new/images/tv/apps/hulu-plus.png',
+                        'buttons': [
+                              {
+                                  'type': 'postback',
+                                  'title': '',
+                                  'payload': 'The selected deal is available for use'
+                              }
+                        ]
                     },
                     {
                         title: 'Chuck E. Cheese',
                         'subtitle': 'Try Chuck E.Cheese amazing Thin and Crispy pizza. Earn 10% cash back on your purchase',
-                        'image_url': 'http://www.bbbsnwfl.org/atf/cf/%7B699040E0-8300-400C-BC25-DBDFE2E82B09%7D/Chuck%20E.%20Cheese%20Building.jpg'
+                        'image_url': 'http://www.bbbsnwfl.org/atf/cf/%7B699040E0-8300-400C-BC25-DBDFE2E82B09%7D/Chuck%20E.%20Cheese%20Building.jpg',
+                        'buttons': [
+                              {
+                                  'type': 'postback',
+                                  'title': '',
+                                  'payload': 'The selected deal is available for use'
+                              }
+                        ]
                     }
                 ]
             }
