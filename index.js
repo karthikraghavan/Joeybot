58ac416a64f66c9deb3bc5da7e13b52ddd882d5a
win10devoffshore <win10devoffshore@hotmail.com>
2016-10-23 15:41:28 -0700

    added request

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/webhook/', function (req, res) {
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
+    console.log(req);
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
